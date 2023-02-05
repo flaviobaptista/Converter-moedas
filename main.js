9e71fdc390f761dffb6e90e1c6b4bfe3311dd859
Flavio de Paulo Baptista <111927624+flaviobaptista@users.noreply.github.com>
2023-02-05 16:56:54 -0300

    Update
    layout

--- main.js
+++ main.js
@@ -1,15 +1,9 @@
-
 function vlop() {
     let real = document.getElementById("vl_real").value;
     let dollar = document.getElementById("vl_dollar").value;
-
-    let resultado = real / dollar;
+    let resultado = real / dollar
 
     document.getElementById("vlresult").innerHTML = resultado
-
+    
 }
-
-
-
-
 
