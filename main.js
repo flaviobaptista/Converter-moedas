02f5e03ff5a32cf04775bb51e4f08d326d6bb9c6
Flavio de Paulo Baptista <111927624+flaviobaptista@users.noreply.github.com>
2023-02-07 18:58:25 -0300

    Update
    Javascript

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
+
+
 function vlop() {
-    let real = document.getElementById("vl_real").value;
-    let dollar = document.getElementById("vl_dollar").value;
+    let real = Number.parseFloat(document.getElementById("vl_real").value);
+    let dollar = Number.parseFloat(document.getElementById("vl_dollar").value);
+
     let resultado = real / dollar
 
     document.getElementById("vlresult").innerHTML = resultado
     
 }
+
+
+
 
